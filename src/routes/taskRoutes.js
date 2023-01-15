0a8e8f0fb1d14da2dfa75a4ef9517e29f30ae05a
Farah Nasreen <farahnasreen2000@gmail.com>
2023-01-15 21:31:04 +0530

    Impl Update and Delete Routes

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -6,6 +6,8 @@ router.get('/get/task/:id', taskController.getTask);
 router.post('/add/task', taskController.addTask);
 router.get('/task/list', taskController.getList);
 router.get('/done/task/:id', taskController.doneTask);
+router.post("/task/update", taskController.update);
+router.post("/task/deleteTask/:id", taskController.deleteTask);
 
 
 
